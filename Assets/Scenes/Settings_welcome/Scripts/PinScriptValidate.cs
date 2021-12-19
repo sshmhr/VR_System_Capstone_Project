62ea8b16c1b6680423a767449b0b7ff530e0dce4
Suyash mehra <sshmhr@gmail.com>
2021-12-18 19:50:55 -0700

    Finished the Frontend for the settings Page

--- Assets/Scenes/Settings_welcome/Scripts/PinScriptValidate.cs
+++ Assets/Scenes/Settings_welcome/Scripts/PinScriptValidate.cs
@@ -1,49 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-
+using UnityEngine.EventSystems;
 //whenever we change the scenes in unity, we need to use this package
 using UnityEngine.UI;
-using System.Linq;
+
 public class PinScriptValidate : MonoBehaviour
 {
-    public string theName;
-    public GameObject inputField;
+    public Slider repSlider;
+    public Text reps;
+    public string passwordEntered;
+    public Text inputField;
     //public GameObject textDisplay;
+    public GameObject soundCheckboxes;
+    public GameObject streamCheckboxes;
     public GameObject objecttoActivate;
     public GameObject objecttoDeactivate;
     public GameObject objecttoActivate1;
     public GameObject objecttoDeactivate1;
-    // gameobjects to be activated or dectivated based on the password entered
-    public string le;
-    public string aah;
-    public string la;
-    public string ta;
-    public string ma;
-    public string ka;
-    public string reps;
-    public string Streamselected;
-    public GameObject le_object;
-    public GameObject aah_object;
-    public GameObject la_object;
-    public GameObject ta_object;
-    public GameObject ma_object;
-    public GameObject ka_object;
-    public GameObject reps_object;
-    public TMPro.TMP_Dropdown myDrop;
-   
 
+    public TMPro.TMP_Dropdown myDrop;
+
+    public void getInputKey()
+    {
+        string name = EventSystem.current.currentSelectedGameObject.name;
+        inputField.text += name;
+    }
     public void ValidatePin()
     {
 
-        theName = inputField.GetComponent<Text>().text;
-        bool isTest = true;
-        
-        if(isTest || theName=="12345")
+        passwordEntered = inputField.text;
+        inputField.text = "";
+        string password = "12345";
+        if (passwordEntered==password)
         {
             objecttoActivate.SetActive(false);
             objecttoDeactivate.SetActive(true);
+
           //this is to activate and deactivate the objects by script
           //once the pin entered is correct, the current page will be deactivated and the control will go to the next screen changeactivites and this screen will be activated
         }
@@ -53,35 +44,31 @@ public class PinScriptValidate : MonoBehaviour
             objecttoDeactivate1.SetActive(true);
           //if the pin entered is wrong, then the control will return back to main menu and main menu screen object will be activated
         }
+
     }
 
     public void SoundSelection()
     {
-        le= le_object.GetComponent<Text>().text;
-        aah = aah_object.GetComponent<Text>().text;
-        la = la_object.GetComponent<Text>().text;
-        ta = ta_object.GetComponent<Text>().text;
-        ma = ma_object.GetComponent<Text>().text;
-        ka = ka_object.GetComponent<Text>().text;
-        Debug.Log(le);
+        Toggle[] checkboxList = soundCheckboxes.GetComponentsInChildren<Toggle>();
+        Debug.Log(checkboxList.Length);
+        foreach (Toggle checkbox in checkboxList) {
+            Debug.Log(checkbox.isOn);
+            Debug.Log(checkbox.name);
+        }
 
   }
 
     public void RepetitionSelection()
     {
-
-        reps = reps_object.GetComponent<Text>().text;
-        Debug.Log(reps);
+        reps.text = "" + repSlider.value;
     }
 
     public void StreamSelection()
-    {   if (myDrop.value == 0)
-            Streamselected = "Long Stream";
+    {
+        Toggle[] checkboxList = streamCheckboxes.GetComponentsInChildren<Toggle>();
+        if (checkboxList[0].isOn) Debug.Log("Longstream");
+        if (checkboxList[1].isOn) Debug.Log("multisyllabic");
 
-        else
-            Streamselected = "Short Stream";
-        
-        Debug.Log(Streamselected);
     }
 }
 
